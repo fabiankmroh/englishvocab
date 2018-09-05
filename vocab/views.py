de9b06483a9667a40174626b0bf4869900252f95
Fabian Roh <fabiankmroh@gmail.com>
2018-09-05 17:41:15 +0900

    Errors

--- vocab/views.py
+++ vocab/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Vocab
 
 def index(request):
-    return HttpResponse('At vocab index')
+    rVocabL = Vocab.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('vocabs/index.html')
+    context = {
+        'rVocabL' : rVocabL,
+    }
+    return HttpResponse(template.render(context, request))
 
 def vocab(request, vocab_id):
     return HttpResponse("Word: %s" % vocab_id)
