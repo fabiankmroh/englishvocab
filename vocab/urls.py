faa8d7fd27e80269a6737cafa2d3ac1c39c76e2b
Fabian Roh <fabiankmroh@gmail.com>
2018-09-11 16:08:02 +0900

    Added Definition Forms & Setup Done for Test Pages

--- vocab/urls.py
+++ vocab/urls.py
@@ -4,7 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:vocab_id>/', views.detail, name = 'detail'),
     path('insert', views.insert, name='insert' ),
     path('vocab_insert', views.vocab_insert, name='vocab_insert'),
+    path('<int:vocab_id>/', views.detail, name = 'detail'),
+    path('<int:vocab_id>/insert', views.detail_insert, name = 'detail_insert'),
+    path('<int:vocab_id>/insert_save', views.detail_insert_save, name = 'detail_insert_save'),
 ]
--- vocab/views.py
+++ vocab/views.py
@@ -5,18 +5,12 @@ from django.utils import timezone
 
 from .models import Vocab
 
+import random
+
 def index(request):
     rVocabL = Vocab.objects.order_by('-pub_date')
     context = { 'rVocabL' : rVocabL }
     return render(request, 'vocab/index.html', context)
-
-def detail(request, vocab_id):
-    vocab = Vocab.objects.get(pk = vocab_id)
-    definition_set = vocab.definition_set.all()
-
-    definition_set.create(defTxt=request.POST['definition'],wrongC = 0)
-
-    return render(request, 'vocab/detail.html', {'vocab':vocab, 'definition_set':definition_set})
 
 def insert(request):
     return render(request, 'vocab/insert.html')
@@ -27,3 +21,24 @@ def vocab_insert(request):
     rVocabL = Vocab.objects.order_by('-pub_date')
     context = { 'rVocabL': rVocabL}
     return render(request, 'vocab/index.html',context)
+
+def detail(request, vocab_id):
+    vocab = Vocab.objects.get(pk= vocab_id)
+    vocabD = vocab.definition_set.all()
+
+    return render(request, 'vocab/detail.html', {'vocab':vocab, 'definition_set': vocabD, 'vocab_id':vocab_id})
+
+def detail_insert(request,vocab_id):
+    return render(request, 'vocab/detail_insert.html',{'vocab_id':vocab_id})
+
+def detail_insert_save(request, vocab_id):
+    vocab = Vocab.objects.get(pk = vocab_id)
+    vocab.definition_set.create(defTxt = request.POST['definition'], wrongC = 0)
+    vocab.save()
+
+    vocabD = vocab.definition_set.all()
+
+    return render(request, 'vocab/detail.html', {'vocab':vocab, 'definition_set': vocabD, 'vocab_id':vocab_id})
+
+def test_start(request):
+    
