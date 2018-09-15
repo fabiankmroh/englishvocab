0b1956ce4b312e1118a81cca2c421325cecc8daa
Fabian Roh <fabiankmroh@gmail.com>
2018-09-15 10:32:37 +0900

    Test Forms Made, Currently Testing & Revising Errors

--- vocab/views.py
+++ vocab/views.py
@@ -20,7 +20,7 @@ def vocab_insert(request):
 
     rVocabL = Vocab.objects.order_by('-pub_date')
     context = { 'rVocabL': rVocabL}
-    return render(request, 'vocab/index.html',context)
+    return render(request, 'vocab/index.html')
 
 def detail(request, vocab_id):
     vocab = Vocab.objects.get(pk= vocab_id)
@@ -41,13 +41,13 @@ def detail_insert_save(request, vocab_id):
     return render(request, 'vocab/detail.html', {'vocab':vocab, 'definition_set': vocabD, 'vocab_id':vocab_id})
 
 def test_start(request):
-    return render(request, 'vocab/test_start.html', {'Vpk':Vpk, 'Dpk':Dpk})
+    return render(request, 'vocab/test_start.html')
 
 def test_form(request):
-    Vpk = random.randint(1,vocab.Objects.all().len())
+    Vpk = random.randint(1,len(Vocab.objects.all()))
     Dpk = 1
     vocab = Vocab.objects.get(pk = Vpk)
-    definition = vocab.definition_set.get(pk = Dpk)
+    definition = vocab.definition_set.filter(pk = Dpk)
     return render(request, 'vocab/test_form.html', {'definition':definition, 'Vpk':Vpk, 'Dpk':Dpk})
 
 def test_submit(request, Vpk, Dpk):
@@ -55,4 +55,3 @@ def test_submit(request, Vpk, Dpk):
     definition = vocab.definition_set.get(pk = Dpk)
     if(request.POST['vocab'] != vocab):
         definition.wrongC += 1
-        
